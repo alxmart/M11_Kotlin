ee7f722c7eeb47efb660cdf681f52b20b1cd6342
alxmart <alxmartinez@gmail.com>
2024-02-18 11:54:53 -0300

    Tests using operators.

--- app/src/main/java/com/luizafmartinez/m11_kotlin/OperAritmRelacLogic.kt
+++ app/src/main/java/com/luizafmartinez/m11_kotlin/OperAritmRelacLogic.kt
@@ -1,4 +1,41 @@
 package com.luizafmartinez.m11_kotlin
 
-class OperAritmRelacLogic {
+fun main() {
+
+    //   +, -, /, * , %
+    val n1 = 10
+    val n2 = 20
+    var resultado = n1 + n2
+    println(resultado)
+    resultado = (2 +2) * 2
+    println(resultado)
+
+    //   >, >=, <, <=, ==, !=
+    var result = 2 > 5
+    println(result) // false
+
+    result = 8 == 5
+    println(result) // false
+
+    result = 8 == 8
+    println(result) // true
+
+    //   && (AND),  || (OR) , ! (NOT)
+
+    result = true || false
+    println(result) // true
+
+    result = 2 > 1 && 5 > 9
+    println(result) // false
+    println(!result) // true
+
+    val idadePessoa = 30
+    val comprasPessoa = 300
+
+    result = idadePessoa >= 50 && comprasPessoa >= 400 // false
+    println(result)
+
+
+
+
 }
