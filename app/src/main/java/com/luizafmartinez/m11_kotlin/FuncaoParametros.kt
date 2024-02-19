04976a8b363727f22618d63e4855c46715a57610
alxmart <alxmartinez@gmail.com>
2024-02-19 11:39:18 -0300

    Tests using functions / parameters.

--- app/src/main/java/com/luizafmartinez/m11_kotlin/FuncaoParametros.kt
+++ app/src/main/java/com/luizafmartinez/m11_kotlin/FuncaoParametros.kt
@@ -1,2 +1,17 @@
 package com.luizafmartinez.m11_kotlin
 
+fun somarv3(num1: Int, num2: Int = 0, num3: Int = 0) { // valor padrão = 0
+
+    println( num1 + num2 )
+
+}
+
+fun main() {
+
+    //somarv3(2,5)
+
+    somarv3(num1 = 3, num2 = 9)
+    somarv3(15) // Resultado: 15( assume valor padrão para num1 e num2)
+
+}
+
